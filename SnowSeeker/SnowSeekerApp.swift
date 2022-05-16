2157ff84eab8dd76f390a6ebb8e23dcdad937b90
warmrome <pxpx786@gmail.com>
2022-05-16 16:22:53 -0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Jameson Hurst on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
