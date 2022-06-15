112774ee8413ef83558069c4dcc1f605265cb38a
warmrome <pxpx786@gmail.com>
2022-06-15 14:25:39 -0700

    initial commit

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,11 +7,95 @@
 
 import SwiftUI
 
+enum SortType {
+    case `default`, alphabetical, country
+}
+
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    @State private var searchText = ""
+    @StateObject var favorites = Favorites()
+    
+    @State private var sortType = SortType.default
+    @State private var showingSortOptions = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(sortedResorts) { resort in
+                NavigationLink {
+                    ResortView(resort: resort)
+                } label: {
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 5)
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("Resorts")
+            .searchable(text: $searchText, prompt: "Search for a resort")
+            .toolbar {
+                Button {
+                    showingSortOptions = true
+                } label: {
+                    Label("Change sort order", systemImage: "arrow.up.arrow.down")
+                }
+            }
+            .confirmationDialog("Sort order", isPresented: $showingSortOptions) {
+                Button("Default") { sortType = .default }
+                Button("Alphabetical") { sortType = .alphabetical }
+                Button("By Country") { sortType = .country }
+            }
+            
+            WelcomeView()
+        }
+        .environmentObject(favorites)
     }
+    
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            return resorts
+        } else {
+            return resorts.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    var sortedResorts: [Resort] {
+        switch sortType {
+        case .default:
+            return filteredResorts
+        case .alphabetical:
+            return filteredResorts.sorted { $0.name < $1.name }
+        case .country:
+            return filteredResorts.sorted { $0.country < $1.country }
+        }
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
